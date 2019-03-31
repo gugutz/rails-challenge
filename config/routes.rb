0d93a495371f25f9c0a7f6f4657e75cfeebd7c16
gugutz <gugutz@gmail.com>
2019-03-31 19:30:38 -0300

    style(layout): add template partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'home#signup'
+  get '/login', to: 'home#login'
+  get '/', to: 'home#index'
+
+  devise_for :clients
+
   resources :histories
   resources :accounts
   resources :clients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/signup', to: 'home#signup'
-  get '/login', to: 'home#login'
-  get '/', to: 'home#index'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # get '/:token', to: 'urls#redirect'
+  # get '/:token', to: 'home#redirect'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_200718) do
+ActiveRecord::Schema.define(version: 2019_03_31_202056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 2019_03_30_200718) do
     t.integer "accounts"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_clients_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true
   end
 
   create_table "histories", force: :cascade do |t|
